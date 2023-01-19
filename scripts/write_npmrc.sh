3ad12dcb5cda2d0462908d9795e0872251589d72
Raphael Huefner <raphaelhuefner@gmail.com>
2023-01-19 14:27:14 -0500

    Try to build docker image and publish to GCP, part 4

--- scripts/write_npmrc.sh
+++ scripts/write_npmrc.sh
@@ -3,9 +3,8 @@
 # Make Bash's error handling strict(er).
 set -o nounset -o pipefail -o errexit
 
-DIRECTORY=$1
+NPMRC_FILEPATH=$1
 GITHUB_TOKEN=$2
 REPOSITORY_OWNER=$3
 
-cd $DIRECTORY
-printf "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@$REPOSITORY_OWNER:registry=https://npm.pkg.github.com" > ./.npmrc
+printf "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@$REPOSITORY_OWNER:registry=https://npm.pkg.github.com" > $NPMRC_FILEPATH
