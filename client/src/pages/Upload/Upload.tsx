3c0f32499bbf0b389c320c53a9ee73a3b8763361
Lance Cummings <lance@glance.ca>
2023-02-17 17:21:16 -0800

    theme styling and uploads page structuring progress

--- client/src/pages/Upload/Upload.tsx
+++ client/src/pages/Upload/Upload.tsx
@@ -72,292 +72,215 @@ const Upload = () => {
   }
 
   return (
-    <Stack px="5rem">
-      <Box pt="2rem" width="50%">
-        <Box>{t('Media Type')}</Box>
-        <Box pt="1rem">
-          <Select
-            label={t('Media Type')}
-            name="type"
-            control={control}
-            fullWidth={false}
-            defaultValue=""
-          >
-            <MenuItem value={ContentTypes.Video}>{t('Video')}</MenuItem>
-            <MenuItem value={ContentTypes.Audio}>{t('Audio')}</MenuItem>
-            <MenuItem value={ContentTypes.PDF}>{t('PDF')}</MenuItem>
-          </Select>
-        </Box>
-      </Box>
-      <Box pt="2rem" width="50%">
-        <Box>{t('What is the Title?')}</Box>
-        <Box>
-          <TextInput
-            control={control}
-            name="title"
-            fullWidth
-            variant="outlined"
-            placeholder={t('Title')}
-          />
-        </Box>
-      </Box>
-      <Box pt="2rem" width="50%">
-        <Box>
-          {t(
-            'You can include an expiration date for content that is only licensed for a period.'
-          )}
-        </Box>
-        <Box pt="1rem">
-          <DatePicker
-            label={t('Expiry Date')}
-            value={value}
-            onChange={(newValue) => {
-              setValue(newValue);
-            }}
-            renderInput={(params) => (
-              <TextField
-                // control={control}
-                // name="expiry"
-                // placeholder={t('Expiry Date')}
-                // fullWidth
-                // variant="outlined"
-                {...params}
+    <Stack>
+        <Grid container spacing={2}>
+          <Grid xs={10} xsOffset={1} md={6} mdOffset={3}>
+            
+            <Box>
+              <Typography component="h2" variant="h2">{t('Media')}</Typography>
+            </Box>
+
+            <Box>
+              <Select
+                label={t('Media Type')}
+                name="type"
+                control={control}
+                fullWidth={false}
+                defaultValue=""
+              >
+                <MenuItem value={ContentTypes.Video}>{t('Video')}</MenuItem>
+                <MenuItem value={ContentTypes.Audio}>{t('Audio')}</MenuItem>
+                <MenuItem value={ContentTypes.PDF}>{t('PDF')}</MenuItem>
+              </Select>
+            </Box>
+
+            <Box>
+              <TextInput
+                control={control}
+                name="title"
+                fullWidth
+                variant="outlined"
+                placeholder={t('Title (required)')}
               />
-            )}
-          />
-        </Box>
-      </Box>
-      <Grid container pt="2rem">
-        <Grid xs={6}>
-          <Stack>
-            <Box>{t('Description')}</Box>
+              <Typography component="p" variant="body2">{t('What is the title of this work? This is the name that people will see when they search for and view your content. Keep this short, you will be able to provide a longer description on the next screen.')}</Typography>
+            </Box>
+
+            <Box>
+              <UploadInput
+                text={t('Media file (required)')}
+                onDrop={handleMediaUpload}
+                maxFiles={1}
+              />
+              <Typography component="p" variant="body2">{t('Upload your video. This is a shared network, to reduce energy and space consumption please upload files xxx, xxx or xxx, no more than xx mb. While your video uploads you can continue filling out the rest of this form. You will be able to submit your entry once the upload has finished.')}</Typography>
+            </Box>
+
+            <Box>
+              <UploadInput
+                text={t('Thumbnail image (required)')}
+                onDrop={handleCoverImageUpload}
+                maxFiles={1}
+              />
+              <Typography component="p" variant="body2">{t('Upload a Thumbnail Image. Recommended image dimensions are xxx by xxx. File size should not exceed xxx kb.')}</Typography>
+            </Box>
+
+            <Box>
+              <TextInput
+                control={control}
+                name="imageText"
+                placeholder={t('Thumbnail Image alt text (required)')}
+                fullWidth
+                variant="outlined"
+              />
+              <Typography component="p" variant="body2">{t('Alt text should describe who or what is in the picture, atmospheric or prop details and any vibrant colours or important design elements or symbols')}</Typography>
+            </Box>
+
             <Box>
               <TextInput
                 control={control}
                 name="description"
-                placeholder={t('Description here')}
+                placeholder={t('Description (required)')}
                 variant="outlined"
                 multiline
                 rows={8}
                 fullWidth
               />
+              <Typography component="p" variant="body2">{t(`Though this text will be accessible to screen reading tools, it's important to remember that different accessibilities are assisted by different kinds of description. Use language in your text that considers a broad audience and is no greater than 280 characters.`)}</Typography>
             </Box>
-          </Stack>
-        </Grid>
-        <Grid xs={6}>
-          <Box pl="2rem">
-            <Typography component="p">{t('Best Practices:')}</Typography>
-            <Typography component="p" pt="1rem">
-              {t(
-                `Though this text will be accessible to screen reading tools, it's important to remember that different accessibilities are assisted by different kinds of description. Use language in your text that considers a broad audience and is no greater than 280 characters.`
-              )}
-            </Typography>
-            <Typography component="p" pt="1rem">
-              {t(
-                'Using your phone or computer record a quick 1-2min description that includes not only the information in the text but a description of where the video is shot, and some of the key visuals in the video.'
-              )}
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
-      <Box>
-        <Button
-          onClick={() => {}}
-          variant="text"
-          endIcon={
-            <Box
-              border="solid"
-              borderRadius="5px"
-              display="inline-block"
-              lineHeight="0"
-            >
-              <FileUploadIcon />
+
+            <Box>
+              <Button
+                onClick={() => {}}
+                variant="text"
+                endIcon={
+                  <Box
+                    border="solid"
+                    borderRadius="5px"
+                    display="inline-block"
+                    lineHeight="0"
+                  >
+                    <FileUploadIcon />
+                  </Box>
+                }
+              >
+                {t('Upload an audio file describing the information above')}
+              </Button>
+              <Typography component="p" variant="body2">{t('Using your phone or computer record a quick 1-2min description that includes not only the information in the text but a description of where the video is shot, and some of the key visuals in the video.')}</Typography>
             </Box>
-          }
-        >
-          {t('Upload an audio file describing the information above')}
-        </Button>
-      </Box>
-      <Box pt="2rem" width="50%">
-        <Box pb="1rem">{t('Upload a Cover Image')}</Box>
-        <UploadInput
-          text={t('Drag photo to upload')}
-          onDrop={handleCoverImageUpload}
-          maxFiles={1}
-        />
-      </Box>
-      <Grid container pt="2rem">
-        <Grid xs={6}>
-          <Box>{t('Type out alt text for the photo above')}</Box>
-          <Box>
-            <TextInput
-              control={control}
-              name="imageText"
-              placeholder={t('Describe the image here')}
-              fullWidth
-              variant="outlined"
-            />
-          </Box>
-        </Grid>
-        <Grid xs={6}>
-          <Box pl="2rem" pt="2rem">
-            {t(
-              'Alt text should describe who or what is in the picture, atmospheric or prop details and any vibrant colors or important design elements or symbols'
-            )}
-          </Box>
-        </Grid>
-      </Grid>
-      <Grid container pt="2rem">
-        <Grid xs={6}>
-          <Box pb="1rem">{t('Upload Your Media File')}</Box>
-          <UploadInput
-            text={t('Drag Media to upload')}
-            onDrop={handleMediaUpload}
-            maxFiles={1}
-          />
-        </Grid>
-        <Grid xs={6}>
-          <Box pl="2rem" pt="2rem">
-            {t(
-              'This is a shared network, to reduce energy and space consumption please upload files X, X or X, no more than X MB.'
-            )}
-          </Box>
-        </Grid>
-      </Grid>
-      <Grid container pt="2rem">
-        <Grid xs={6}>
-          <Box>
-            {t(
-              'Upload VTT File For Subtitles (use multiple fields for different language files)'
-            )}
-          </Box>
-          <Box pt="1rem">
-            <UploadInput
-              text={t('Drag VTT file(s) to upload')}
-              onDrop={handleVTTFilesUpload}
-              maxFiles={1}
-            />
-          </Box>
-        </Grid>
-        <Grid xs={6}>
-          <Box pl="2rem" pt="2rem">
-            {t(
-              'VTT files ensure your content has subtitles. Closed captions include information about background sounds and speaker changes. Subtitles assume the viewer hears the audio and as a result does not contain background information. Please format the file to have this code at the top to ensure it adapts to various screen sizes'
-            )}
-            <br />
-            {t('Code')}: XXX
-          </Box>
-        </Grid>
-      </Grid>
-      <Box pt="2rem" width="50%">
-        <Grid container>
-          <Grid xs={6}>
-            <Box>{t('Add a Sign-language Option To Your Video')}</Box>
-            <Box pt="1rem">
+
+            <Box>
+              <UploadInput
+                text={t('VTT file (required)')}
+                onDrop={handleVTTFilesUpload}
+                maxFiles={1}
+              />
+              <Typography component="p" variant="body2">
+                {t('Upload VTT File For Subtitles. VTT files ensure your content has subtitles. Closed captions include information about background sounds and speaker changes. Subtitles assume the viewer hears the audio and as a result does not contain background information. Please format the file to have this code at the top to ensure it adapts to various screen sizes')}
+                <br />
+                {t('Code')}: XXX
+              </Typography>
+            </Box>
+
+            <Box>
+              <DatePicker
+                label={t('Expiry Date')}
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    // control={control}
+                    // name="expiry"
+                    // placeholder={t('Expiry Date')}
+                    // fullWidth
+                    // variant="outlined"
+                    {...params}
+                  />
+                )}
+              />
+              <Typography component="p" variant="body2">{t('You can include an expiration date for content that is only licensed for a period.')}</Typography>
+            </Box>
+
+            <Box>
+              <Typography component="h2" variant="h2">{t('Accessibility')}</Typography>
+              <Typography component="h5" variant="h5">{t('Add a Sign-language Option To Your Video')}</Typography>
+              <Typography component="p">{t('We encourage everyone to add sign language to their video content. Cube has a partnership with Deaf Spectrum who is able to book Signers willing to be videoed for content online. This does not need to be uploaded at the same time your content is. If you book using the button here, which is a portal to Deaf Spectrum’s site, signers are given the time they need to produce video content and the video is added later as a picture on picture option for users who select it.')}</Typography>
               <Button>{t('Book with Deaf Spectrum')}</Button>
             </Box>
-          </Grid>
-          <Grid xs={6}>
-            <Box pl="2rem" pt="2rem">
-              {t(
-                'We encourage everyone to add sign language to their video content. Cube has a partnership with Deaf Spectrum who is able to book Signers willing to be videoed for content online. This does not need to be uploaded at the same time your content is. If you book using the button here, which is a portal to Deaf Spectrum’s site, signers are given the time they need to produce video content and the video is added later as a picture on picture option for users who select it.'
-              )}
+            
+            <Typography component="h2" variant="h2">{t('Tags')}</Typography>
+
+            <Box>
+              <ChipInput
+                control={control}
+                name="collaborators"
+                fullWidth
+                chips={[
+                  'Live Performance',
+                  'Workshop',
+                  'Kids Workshop',
+                  'Artist Talk',
+                  'Language Teaching',
+                  'Cultural Teaching',
+                  'Studio Visit',
+                  'Gathering',
+                  'Book Launch',
+                  'Subtitles',
+                  'Sign Language',
+                  'Multilingual Subtitles',
+                  'Tsleil-Waututh',
+                  'kʷakʷəkʲəʔwakʷ',
+                  'Sḵwx̱wúʔmesh',
+                  'Anishinaabe',
+                  'French',
+                  'Chinese',
+                  'BC',
+                  'Alberta',
+                  'Québec',
+                  'Regina',
+                  'Manitoba',
+                  'Toronto'
+                ]}
+              />
+              <Typography component="p" variant="body2">{t('Separate tags by comma. Tags are our way of expanding the terms by which users can search content. They also contribute to your govenance token holdings by expressing how inclusive and accessible your content is. If you want content to be bundled under an exhibition name please create that tag:')}</Typography>
             </Box>
+              
+            <Box>
+              <Typography component="p" variant="body2">{t('Credit any individuals who contribute to this content, including artists, editors etc. Add as many contributors as needed, using “Add more”. Only one name per field.')}</Typography>
+              <TextInput
+                control={control}
+                name="contributor"
+                fullWidth
+                variant="outlined"
+                placeholder={t('e.g. Editor -> Editor Name')}
+              />
+              <Button variant="text">{t('+ add more')}</Button>
+            </Box>
+            
+            <Box>
+              <ChipInput
+                control={control}
+                name="collaborators"
+                fullWidth
+                // chips={collaborators || []}
+                chips={[]}
+              />
+              <Typography component="p" variant='body2'>{t('Did you work with another organization? Include them in the credits. Separate collaborators by comma.')}</Typography>
+            </Box>
+
+            <Box>
+              <Button
+                onClick={handleSubmit(onSubmit)}
+                fullWidth={false}
+                disabled={isLoading}
+              >
+                {t('Submit')}
+              </Button>
+            </Box>
+            
           </Grid>
         </Grid>
-      </Box>
-      <Typography variant="h4" component="h4" pt="2rem">
-        {t('Collaborators')}
-      </Typography>
-      <Box pt="2rem" width="50%">
-        <Box>
-          {t(
-            'Credit any organizations on Cube who contributed to this content'
-          )}
-        </Box>
-        <Box>
-          <ChipInput
-            control={control}
-            name="collaborators"
-            fullWidth
-            // chips={collaborators || []}
-            chips={[]}
-          />
-        </Box>
-      </Box>
-      <Box pt="2rem" width="50%">
-        <Box>
-          {t(
-            'Credit any individuals who contribute to this content, including artists, editors, etc (they do not need to have a profile on Cube):'
-          )}
-        </Box>
-        <Box>
-          <TextInput
-            control={control}
-            name="contributor"
-            fullWidth
-            variant="outlined"
-            placeholder={t('e.g. Editor -> Editor Name')}
-          />
-        </Box>
-        <Box>
-          <Button variant="text">{t('+ add more')}</Button>
-        </Box>
-      </Box>
-      <Typography variant="h4" component="h4" pt="2rem">
-        {t('Add Your Content Tags')}
-      </Typography>
-      <Box pt="2rem" width="50%">
-        <Box>
-          {t(
-            'Tags are our way of expanding the terms by which users can search content. They also contribute to your govenance token holdings by expressing how inclusive and accessible your content is. If you want content to be bundled under an exhibition name please create that tag:'
-          )}
-        </Box>
-        <Box>
-          <ChipInput
-            control={control}
-            name="collaborators"
-            fullWidth
-            chips={[
-              'Live Performance',
-              'Workshop',
-              'Kids Workshop',
-              'Artist Talk',
-              'Language Teaching',
-              'Cultural Teaching',
-              'Studio Visit',
-              'Gathering',
-              'Book Launch',
-              'Subtitles',
-              'Sign Language',
-              'Multilingual Subtitles',
-              'Tsleil-Waututh',
-              'kʷakʷəkʲəʔwakʷ',
-              'Sḵwx̱wúʔmesh',
-              'Anishinaabe',
-              'French',
-              'Chinese',
-              'BC',
-              'Alberta',
-              'Québec',
-              'Regina',
-              'Manitoba',
-              'Toronto'
-            ]}
-          />
-        </Box>
-      </Box>
-      <Stack py="2rem">
-        <Box>
-          <Button
-            onClick={handleSubmit(onSubmit)}
-            fullWidth={false}
-            disabled={isLoading}
-          >
-            {t('Ready to upload Video to Main Page')}
-          </Button>
-        </Box>
-      </Stack>
+
     </Stack>
   );
 };
--- client/src/theme/index.ts
+++ client/src/theme/index.ts
@@ -2,14 +2,19 @@ import { Settings } from '@mui/icons-material';
 import { createTheme, responsiveFontSizes } from '@mui/material';
 
 const DEFAULT_BORDER_RADIUS = 5;
-const DEFAULT_FONT_SIZE = 18;
-const FONT_FAMILY = ['Arimo'].join(',');
+const DEFAULT_FONT_SIZE = 16;
+const FONT_FAMILY = ['Inter'].join(',');
 const DEFAULT_THEME_MODE = 'dark';
 
 const THEME_COLORS = {
   primary: {
-    main: '#baf2ef',
-    // background: 'red',
+    main: '#95F5CB',
+    light: '#D9FFEE',
+    dark: '#57838b',
+  },
+  background: {
+    default: '#28343C',
+    paper: '#2f4048',
   }
 };
 //*// primary dark and primary light are used by folks primary light and dark Settings. But I would like to set background color default here as well. Lance what do you think?//
@@ -22,15 +27,91 @@ const getTheme = (mode?: 'dark' | 'light') =>
       },
       typography: {
         fontFamily: FONT_FAMILY,
+        fontWeightLight: 400,
         fontSize: DEFAULT_FONT_SIZE,
+        h1: {
+          fontFamily: 'Rubik',
+          fontSize: '3.8rem',
+          fontWeight: 400,
+          color: THEME_COLORS.primary.main,
+        },
+        h2: {
+          fontFamily: 'Rubik',
+          fontSize: '3.5rem',
+          color: THEME_COLORS.primary.main,
+        },
+        h3: {
+          fontFamily: 'Rubik',
+          fontSize: '2rem',
+          color: THEME_COLORS.primary.main,
+        },
+        h4: {
+          fontFamily: 'Rubik',
+          fontSize: '1.5rem',
+          color: THEME_COLORS.primary.main,
+        },
+        h5: {
+          fontSize: '1.125rem',
+          fontWeight: 700,
+        },
+        h6: {
+          fontSize: '1rem',
+          fontWeight: 700,
+        },
+        button: {
+          fontSize: '1rem',
+          fontWeight: 700,
+        },
+        caption: {
+          fontSize: '1rem',
+        },
+        overline: {
+          fontSize: '1rem',
+          fontWeight: 600,
+        },
+        body1: {
+          fontSize: '1.125rem',
+          margin: '1.875rem 0'
+        },
         body2: {
-          padding: '1.5rem 0'
+          fontSize: '1rem',
+          margin: '1.875rem 0'
         }
       },
       palette: {
         mode: mode || DEFAULT_THEME_MODE,
         primary: {
-          main: THEME_COLORS.primary.main
+          main: THEME_COLORS.primary.main,
+          light: THEME_COLORS.primary.light,
+          dark: THEME_COLORS.primary.dark,
+        },
+        secondary: {
+          main: THEME_COLORS.primary.main,
+          light: THEME_COLORS.primary.light,
+          dark: THEME_COLORS.primary.dark,
+        },
+        background: {
+          default: THEME_COLORS.background.default,
+          paper: THEME_COLORS.background.paper,
+        },
+        text: {
+          primary: THEME_COLORS.primary.light,
+          secondary: THEME_COLORS.primary.main,
+          disabled: THEME_COLORS.primary.dark,
+        },
+        error: {
+          main: '#c01534',
+        },
+        warning: {
+          main: '#ffde33',
+        },
+        success: {
+          main: THEME_COLORS.primary.main,
+          light: THEME_COLORS.primary.light,
+          dark: THEME_COLORS.primary.dark,
+        },
+        info: {
+          main: '#75b9f7',
         }
       },
       components: {
