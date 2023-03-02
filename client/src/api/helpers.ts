7db0f1cb5f182beb249e0db0b473cab41e4d03ff
Raphael Huefner <raphaelhuefner@gmail.com>
2023-03-02 13:17:34 -0500

    Fix buld error after merging main

--- client/src/api/helpers.ts
+++ client/src/api/helpers.ts
@@ -1,7 +1,7 @@
-import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, UPLOADS_API_PATH } from './constants';
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } from './constants';
 import { PaginationQueryKeys, ContentQueryKeys } from './enums';
-import { Upload } from 'tus-js-client';
-import { contentFilesApi } from './httpClient';
+import { Upload, HttpRequest, HttpResponse } from 'tus-js-client';
+import { UPLOAD_TUS_ENDPOINT, uploadApi } from './httpClient';
 
 type QueryKeys = typeof ContentQueryKeys;
 
@@ -43,43 +43,53 @@ export const blobToBase64 = (file: File) =>
     reader.onerror = (error) => reject(error);
   });
 
-export const upload = async (file: File) => {
-  // get the upload url
-  const uploadUrlApi = await contentFilesApi.contentUploadUrl(`${file.size}`);
-  const response = await uploadUrlApi();
-  const uploadUrl = response.headers['location'];
 
-  const upload = new Upload(file, {
-    endpoint: uploadUrl,
-    retryDelays: [0, 3000, 5000, 10000, 20000],
-    metadata: {
-      filename: file.name,
-      filetype: file.type
-    },
-    onError: function (error: Error) {
-      console.log('Failed because: ' + error);
-    },
-    onProgress: function (bytesUploaded: number, bytesTotal: number) {
-      const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
-      console.log(bytesUploaded, bytesTotal, percentage + '%');
-    },
-    onSuccess: function () {
-      console.log(
-        'Download %s from %s',
-        (upload.file as File).name,
-        upload.url
-      );
+export type ProgressHandler = (bytesUploaded: number, bytesTotal: number) => void;
+
+const defaultProgressHandler: ProgressHandler = (bytesUploaded: number, bytesTotal: number) => {
+    const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
+    console.log(`${bytesUploaded}/${bytesTotal} = ${percentage}%`);
+}
+
+export const uploadViaTus = async (file: File, headers: any, meta: any, progressHandler: ProgressHandler = defaultProgressHandler):Promise<string | undefined> => {
+  return await new Promise((resolve, reject) => {
+    let fileId: string | undefined = undefined;
+
+    const options = {
+      endpoint: UPLOAD_TUS_ENDPOINT,
+      headers,
+      retryDelays: [0, 3000, 5000, 10000, 20000],
+      metadata: {
+        fileName: file.name,
+        mimeType: file.type,
+        ...meta
+      },
+      onAfterResponse(req: HttpRequest, res: HttpResponse) {
+        if (req.getURL() === UPLOAD_TUS_ENDPOINT) {
+          fileId = res.getHeader("CUBE-File-Id");
+          resolve(fileId); // Resolve early, so we can get on with creating content.
+        }
+      },
+      onError(error: any) {
+        reject(error);
+      },
+      onProgress: progressHandler,
+      onSuccess() {
+        resolve(fileId);
+      },
     }
-  });
 
-  // Check if there are any previous uploads to continue.
-  upload.findPreviousUploads().then(function (previousUploads) {
-    // Found previous uploads so we select the first one.
-    if (previousUploads.length) {
-      upload.resumeFromPreviousUpload(previousUploads[0]);
-    }
+    const upload = new Upload(file, options);
 
-    // Start the upload
-    upload.start();
+    // Check if there are any previous uploads to continue.
+    upload.findPreviousUploads().then(function (previousUploads) {
+      // Found previous uploads so we select the first one. 
+      if (previousUploads.length) {
+        upload.resumeFromPreviousUpload(previousUploads[0]);
+      }
+
+      // Start the upload
+      upload.start();
+    })
   });
 };
