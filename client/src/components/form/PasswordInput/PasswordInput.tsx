46115b60a084e97beaa376db619cd70d96c90c8a
Lance Cummings <lance@glance.ca>
2023-03-30 17:33:59 -0700

    adds password visibility toggle to form password inputs

--- client/src/components/form/PasswordInput/PasswordInput.tsx
+++ client/src/components/form/PasswordInput/PasswordInput.tsx
@@ -1,15 +1,40 @@
+import { InputAdornment, IconButton } from '@mui/material';
 import { t } from 'i18next';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useState } from "react";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { alpha } from '@mui/material/styles';
 
 import TextInput from '../TextInput';
 import { TextInputProps } from '../TextInput/types';
 
 const PasswordInput: FC<TextInputProps> = (props) => {
   const { t } = useTranslation();
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+
   return (
     <TextInput
-      type="password"
+      type={passwordShown ? "text" : "password"}
+      
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={togglePassword}
+            >
+              {passwordShown ? <Visibility style={{ fill: alpha('#D9FFEE', 0.5) }} /> : <VisibilityOff style={{ fill: alpha('#D9FFEE', 0.5) }} />}
+            </IconButton>
+          </InputAdornment>
+        )
+      }}
+
       rules={{
         minLength: { value: 8, message: t('Minimum password length is 8') },
         maxLength: { value: 72, message: t('Maximum password length is 72') },
