ede30b464d3bf5e75d43bf580c1c4fa01d019114
taylorjackson <taylorjackson@gamestop.ca>
2023-04-06 15:40:56 -0700

    WIP: updated bff with status and data endpoints, passing of security headers

--- services/bff/src/index.ts
+++ services/bff/src/index.ts
@@ -10,79 +10,80 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/upload/video-tus-reservation', allowIfAnyOf('contentEditor'), async (req: Request, res: Response) => {
-  const upload = await cloudflareApi.post('/upload/video-tus-reservation', req.body);
-  res.json(upload.data);
+  const { status, data } = await cloudflareApi.post('/upload/video-tus-reservation', req.body, { headers: req.headers });
+  res.status(status).json(data);
 });
 
 app.post('/upload/s3-presigned-url', allowIfAnyOf('contentEditor'), async (req: Request, res: Response) => {
-  const upload = await cloudflareApi.post('/upload/s3-presigned-url', req.body);
-  res.json(upload.data);
+  const { status, data } = await cloudflareApi.post('/upload/s3-presigned-url', req.body, { headers: req.headers });
+  res.status(status).json(data);
 });
 
 app.post('/content', allowIfAnyOf('contentEditor'), async (req: Request, res: Response) => {
-  const id = await contentApi.get('content/', req.body);
-  res.json(id.data);
+  const { status, data } = await contentApi.post('content/', req.body, { headers: req.headers });
+  res.status(status).json(data);
 });
 
 app.get('/content', async (req: Request, res: Response) => {
-  const content = await contentApi.get('content/');
-  res.json(content.data);
+  const { status, data } = await contentApi.get('content/');
+  res.status(status).json(data);
 });
 
 app.get('/content/:contentId', async (req: Request, res: Response) => {
-  const content = await contentApi.get('content/' + req.params.contentId);
-  res.json(content.data);
+  const { status, data } = await contentApi.get('content/' + req.params.contentId);
+  res.status(status).json(data);
 });
 
 app.post('/auth/user', allowIfAnyOf('anonymous', 'userAdmin'), async (req: Request, res: Response) => {
-  const user = await identityApi.post('auth/user', req.body);
-  res.json(user.data);
+  const { status, data } = await identityApi.post('auth/user', req.body, { headers: req.headers });
+  res.status(status).json(data);
 });
 
 app.post('/auth/login', async (req: Request, res: Response) => {
-  const login = await identityApi.post('auth/login', req.body);
-  res.json(login.data);
+  const { status, data } = await identityApi.post('auth/login', req.body);
+  res.status(status).json(data);
 });
 
 app.post('/auth/anonymous', async (req: Request, res: Response) => {
-  const anon = await identityApi.post('auth/anonymous', req.body);
-  res.json(anon.data);
+  const { status, data } = await identityApi.post('auth/anonymous', req.body);
+  res.status(status).json(data);
 });
 
 app.put('/auth/email', allowIfAnyOf('active'), async (req: Request, res: Response) => {
-  const email = await identityApi.put('auth/email', req.body);
-  res.json(email.data);
+  const { status, data } = await identityApi.put('auth/email', req.body, { headers: req.headers });
+  res.status(status).json(data);
 });
 
 app.put('/auth/password', allowIfAnyOf('active'), async (req: Request, res: Response) => {
-  const password = await identityApi.put('auth/password', req.body);
-  res.json(password.data);
+  const { status, data } = await identityApi.put('auth/password', req.body, { headers: req.headers });
+  res.status(status).json(data);
 });
 
 app.get('/auth/verify', allowIfAnyOf('unverified'), async (req: Request, res: Response) => {
-  const verify = await identityApi.get('auth/verify', {
-    params: req.query
+  const { status, data } = await identityApi.get('auth/verify', {
+    params: req.query,
+    headers: req.headers
   });
 
-  res.json(verify.data);
+  res.status(status).json(data);
 });
 
 app.get('/auth/forgot-password', async (req: Request, res: Response) => {
-  const forgotPassword = await identityApi.get('auth/forgot-password', {
+  const { status, data } = await identityApi.get('auth/forgot-password', {
     params: req.query
   });
 
-  res.json(forgotPassword.status);
+  res.status(status).json(data);
 });
 
 app.post('/profiles', async (req: Request, res: Response) => {
-  const id = await profileApi.post('profiles/', req.body);
-  res.json(id.data);
+  const { status, data } = await profileApi.post('profiles/', req.body);
+  res.status(status).json(data);
 });
 
 app.get('/profiles/:id', async (req: Request, res: Response) => {
-  const profile = await profileApi.get('profiles/' + req.params.id);
-  res.json(profile.data);
+  const { status, data } = await profileApi.get('profiles/' + req.params.id);
+  res.status(status).json(data);
 });
 
 app.listen(settings.PORT, async () => {
