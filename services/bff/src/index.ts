fcdd3f48907a17e421a27812d57d655743127cc8
Raphael Huefner <raphaelhuefner@gmail.com>
2023-04-24 22:49:54 -0400

    prep profile page API

--- services/bff/src/index.ts
+++ services/bff/src/index.ts
@@ -6,7 +6,7 @@ import { AxiosHeaders } from 'axios';
 import * as settings from './settings';
 import { allowIfAnyOf } from './auth';
 import { identityApi, profileApi, contentApi, cloudflareApi } from './microservices';
-import { filterObject } from './utils';
+import { inspect, filterObject } from './utils';
 
 const app: Express = express();
 app.use(cors());
@@ -57,16 +57,93 @@ app.post('/content', allowIfAnyOf('contentEditor'), async (req: Request, res: Re
 });
 
 app.get('/content', async (req: Request, res: Response) => {
-  const { status, data } = await contentApi.get('content', {
-    params: req.query,
-    headers: filterHeadersToForward(req, 'authorization')
+  const {
+    profileId,
+    page,
+    page_size,
+    category,
+    type,
+    nation,
+    creator
+  } = req.query;
+
+  // Temporarily (while working on profile page) respond with the same mock data as content microservice used to do.
+  // TODO Transform data returned from content MS to the output format of this mock
+  res.status(200).json({
+    "data": [
+      {
+        "id": "1",
+        "title": "Title 1",
+        "creator": "Creator 1",
+        "url": "/content/1",
+        "thumbnailUrl": "images/video_thumbnail.jpg",
+        "iconUrl": "images/creator_icon.png",
+        "category": "video",
+        "type": "video"
+      },
+      {
+        "id": "2",
+        "title": "Title 2",
+        "creator": "Creator 2",
+        "url": "/content/2",
+        "thumbnailUrl": "images/video_thumbnail.jpg",
+        "iconUrl": "images/creator_icon.png",
+        "category": "video",
+        "type": "video"
+      }
+    ]
   });
-  res.status(status).json(data);
+
+  // const { status, data } = await contentApi.get('content', {
+  //   params: req.query,
+  //   headers: filterHeadersToForward(req, 'authorization')
+  // });
+  // res.status(status).json(data);
 });
 
 app.get('/content/:contentId', async (req: Request, res: Response) => {
-  const { status, data } = await contentApi.get('content/' + req.params.contentId);
-  res.status(status).json(data);
+  // Temporarily (while working on profile page) respond with the same mock data as content microservice used to do.
+  // TODO Transform data returned from content MS to the output format of this mock
+  res.status(200).json({
+    "data": {
+      "id": req.params.contentId,
+      "url": "/video.mp4",
+      "title": `Video ${req.params.contentId}`,
+      "createdDate": "07/01/2022",
+      "updatedDate": "07/01/2022",
+      "description": "Description of content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor sem faucibus auctor quam pretium massa nulla cursus. Vel, a nisl ipsum, nisl. Mauris.",
+      "descriptionUrl": "/description.mp3",
+      "credits": "Dawn Powell, Camera Operator, Alissa Cat, Public Programs Magnus Ten, Editor",
+      "contributors": [
+        {
+          "id": "1",
+          "link": "/profile/1",
+          "name": "Museum Of Anthropology",
+          "socialUrl": "https: //www.twitter.com",
+          "socialHandle": "@Moa",
+          "logoUrl": "/images/moa.svg"
+        },
+        {
+          "id": "2",
+          "name": "Museum of Vancouver",
+          "socialUrl": "https: //www.twitter.com",
+          "socialHandle": "@Mov",
+          "logoUrl": ""
+        },
+        {
+          "id": "3",
+          "name": "Dana Claxton"
+        }
+      ],
+      "tags": [
+        "tag 1",
+        "tag 2"
+      ]
+    }
+  });
+
+  // const { status, data } = await contentApi.get('content/' + req.params.contentId);
+  // res.status(status).json(data);
 });
 
 app.post('/auth/user', allowIfAnyOf('anonymous', 'userAdmin'), async (req: Request, res: Response) => {
@@ -116,9 +193,116 @@ app.post('/profiles', async (req: Request, res: Response) => {
   res.status(status).json(data);
 });
 
+// TODO Make this an API endpoint in the cloudflare service
+const getFiles = async (fileIds: string[]) => {
+  const files: { [k: string]: any } = {};
+  const errors: any[] = [];
+  for (const fileId of fileIds) {
+    const { status, data } = await cloudflareApi.get(`files/${fileId}`);
+    if (200 == status) {
+      files[fileId] = data;
+    } else {
+      errors.push({ fileId, status, data });
+    }
+  }
+  return { files, errors };
+}
+
+const transformContentListForProfile = async (profile:any, contentList:any[]) => {
+  const fieldNames = {
+    coverImageFileId: 'coverImageUrl',
+    mediaFileId: 'mediaUrl',
+    subtitlesFileId: 'subtitlesUrl',
+    transcriptFileId: 'transcriptUrl'
+  };
+
+  const fileIds = [];
+  for (const content of contentList) {
+    for (const fileIdFieldName of Object.keys(fieldNames)) {
+      if (content[fileIdFieldName]) {
+        fileIds.push(content[fileIdFieldName]);
+      }
+    }
+  }
+
+  const { files, errors: fileErrors } = await getFiles(fileIds);
+  if (fileErrors.length) {
+    inspect(`in transformContentListForProfile(${profile.id}):`, { fileErrors });
+  }
+
+  for (const content of contentList) {
+    for (const [fileIdFieldName, urlFieldName] of Object.entries(fieldNames)) {
+      if (content[fileIdFieldName] && files[content[fileIdFieldName]]) {
+        const file = files[content[fileIdFieldName]];
+
+        // Videos have both, `dashUrl` or `hlsUrl`. TBD Which one works better for FE?
+        const url = file.playerInfo.publicUrl ? file.playerInfo.publicUrl : file.playerInfo.dashUrl;
+
+        content[urlFieldName] = url;
+      }
+    }
+
+    content.creator = profile.organization;
+    content.iconUrl = profile.logoUrl;
+
+    // TODO Remove hack after migrating existing content to add `category` field.
+    if (!content.category) {
+      content.category = content.type;
+    }
+  }
+
+  const contentByCategory: { [k: string]: any[] } = {};
+  for (const content of contentList) {
+    if (!contentByCategory[content.category]) {
+      contentByCategory[content.category] = [];
+    }
+    contentByCategory[content.category].push(content);
+  }
+  return Object.entries(contentByCategory).map((category, content) => ({ category, content }));
+}
+
 app.get('/profiles/:id', async (req: Request, res: Response) => {
-  const { status, data } = await profileApi.get('profiles/' + req.params.id);
-  res.status(status).json(data);
+  const profileId = req.params.id;
+  const { status, data: profileDetail } = await profileApi.get('profiles/' + profileId);
+  if (200 != status) {
+    return res.status(status).json(profileDetail);
+  }
+
+  const profile = profileDetail.data;
+
+  const { files, errors: fileErrors } = await getFiles([profile.heroFileId, profile.logoFileId, profile.descriptionAudioFileId]);
+  if (fileErrors.length) {
+    console.log({ profile, fileErrors });
+    // TODO prevent internal details from leaking through the error messages 
+    // return res.status(500).json(fileErrors);
+  }
+
+  if (files[profile.heroFileId]) {
+    profile.heroUrl = files[profile.heroFileId].playerInfo.publicUrl;
+  }
+  if (files[profile.logoFileId]) {
+    profile.logoUrl = files[profile.logoFileId].playerInfo.publicUrl;
+  }
+  if (files[profile.descriptionAudioFileId]) {
+    profile.descriptionAudioUrl = files[profile.descriptionAudioFileId].playerInfo.publicUrl;
+  }
+
+  const { status: contentStatus, data: content } = await contentApi.get('content', {
+    params: {
+      profileId: profile.id,
+
+      // TODO proper pagination on the profile page
+      offset: 0,
+      limit: 100,
+    }
+  });
+  if (200 != contentStatus) {
+    return res.status(contentStatus).json(content);
+  }
+
+  profile.content = transformContentListForProfile(profile, content.data);
+
+  res.status(200).json({ data: profile });
 });
 
 app.get('/', async (req: Request, res: Response) => {
