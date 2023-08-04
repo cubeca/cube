38b97bbe6cf91e0ed88d5c6ce17a2072001ede67
Jonathan Bluks <jon.bluks@gmail.com>
2023-08-04 16:27:01 -0700

    Update Radio Buttons

--- client/src/components/form/RadioInput/RadioInput.styled.tsx
+++ client/src/components/form/RadioInput/RadioInput.styled.tsx
@@ -2,12 +2,15 @@ import { styled } from 'theme/utils';
 import RadioInput from './RadioInput';
 
 export const DarkRadioInput = styled(RadioInput)`
-  & label {
+  & label, .Mui-focused {
     color: ${(props) => props.theme.palette.background.default};
     display: block;
     font-size: 1rem;
     font-weight: 500;
     margin-bottom: 0;
     font-weight: 600;
+  }
+  .MuiRadio-root {
+    color: ${(props) => props.theme.palette.background.default};
   }
 `;
