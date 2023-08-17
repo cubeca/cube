43ab5b165d5c5ff7892f8ac94e504ad40093d4e7
Lance Cummings <lance@glance.ca>
2023-08-17 16:38:53 -0700

    login page responsive styling

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,11 +1,13 @@
-import { Box, Grid, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+import Grid from '@mui/system/Unstable_Grid';
+import Footer from 'components/layout/Footer';
 import Button from 'components/Button';
 import { useTranslation } from 'react-i18next';
 import { useLocation, useNavigate } from 'react-router-dom';
-import LoginBackground from 'assets/images/login-signup-background.jpg';
 import { LoginForm } from './components/LoginForm';
 import { CreatorSignupForm } from './components/CreatorSignupForm';
 import { UserSignupForm } from './components/UserSignupForm';
+import * as s from './Login.styled';
 
 const Login = () => {
   const { t } = useTranslation('common');
@@ -25,51 +27,34 @@ const Login = () => {
 
   return (
     <Stack>
-      <Grid
-        container
-        spacing={0}
-        columnGap="none"
-        direction="row"
-        xs={12}
-        md={12}
-        flex-direction={{ xs: 'column', sm: 'column', md: 'row' }}
-        order={{ xs: 'reverse' }}
-      >
-        <Grid item xs={12} md={6} justifyContent={{ xs: 'center' }}>
-          <Box padding={12}>
+      <Grid container>
+        <Grid xs={10} xsOffset={1} md={5}>
+          
+          <s.FormWrapper>
             {isLogin ? <LoginForm emailVerified={emailVerified} passwordReset={passwordReset} /> : signUpForm}
-          </Box>
+          </s.FormWrapper>
+
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Box
-            p={0}
-            component="div"
-            sx={{
-              backgroundImage: `
-              linear-gradient(to right, rgba(149, 245, 203, 0.82), rgba(149, 245, 203, 0.32)),
-              url('${LoginBackground}')`,
-              backgroundSize: 'cover'
-            }}
-            height="100%"
-            width="100%"
-            display="flex"
-            alignItems="center"
-          >
-            <Box pl={12}>
+        <Grid xs={12} md={6}>
+          
+          <s.CTAWrapper>
+            <s.CTAMessage>
               <Typography component="h2" variant="h2" color="secondary">
                 {isLogin ? t('New Here?') : t('Already have an account?')}
               </Typography>
+
               {isLogin && (
                 <Typography
                   component="h4"
                   variant="h4"
                   color="secondary"
-                  pb={4}
                 >
                   {t('Sign up and Start Contributing')}
                 </Typography>
               )}
-              <Box display="flex" gap="10px">
+
+              <s.LinkWrapper>
+                
                 <Button
                   color="secondary"
                   onClick={() =>
@@ -78,6 +63,7 @@ const Login = () => {
                 >
                   {isLogin ? t('User Sign up') : t('Login')}
                 </Button>
+
                 {isLogin && (
                   <Button
                     color="secondary"
@@ -88,11 +74,16 @@ const Login = () => {
                     {isLogin ? t('Creator Sign up') : t('Login')}
                   </Button>
                 )}
-              </Box>
-            </Box>
-          </Box>
+
+              </s.LinkWrapper>
+
+            </s.CTAMessage>
+          </s.CTAWrapper>
+
         </Grid>
       </Grid>
+
+      <Footer />
     </Stack>
   );
 };
