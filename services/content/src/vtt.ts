c988ad6b7742d58000e070a2c644a4cabad31d45
Adam Skalicky <adam.skalicky@scencdn.com>
2023-10-02 20:13:27 -0700

    Working VTT code

--- services/content/src/vtt.ts
+++ services/content/src/vtt.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 const ffmpeg = require('ffmpeg');
 import OpenAI from 'openai';
 
+const openaiKey = '';
 const maxNoSpeechConfidence = 0.6;
 
 const transcribe = async (audioPath: string) => {
@@ -33,7 +34,29 @@ const transcribe = async (audioPath: string) => {
   return finalResponse;
 };
 
-const generateVtt = async (mediaURL: string) => {
+const generateVTT = (transcript: any) => {
+  let vtt = 'WEBVTT\n\n';
+  for (const key in transcript) {
+    const { text, start, end } = transcript[key];
+    //convert start and end (which are in seconds from media start) to HH:MM:SS.MS
+    //H,M,S, are all 2 digits
+
+    const formatTime = (time: number) => {
+      const hours = String(Math.floor(time / 3600)).padStart(2, '0');
+      const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, '0');
+      const seconds = String(Math.floor((time % 3600) % 60)).padStart(2, '0');
+      const milliseconds = String(Math.floor((time % 1) * 1000)).padStart(3, '0');
+      return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    };
+    const startFormatted = formatTime(start);
+    const endFormatted = formatTime(end);
+    vtt += `${startFormatted} --> ${endFormatted}\n`;
+    vtt += `${text}\n\n`;
+  }
+  return vtt;
+};
+
+const transcribeVideo = async (mediaURL: string) => {
   const id = Date.now().toString();
   const outputPath = './tmp';
 
@@ -64,7 +87,10 @@ const generateVtt = async (mediaURL: string) => {
       });
     });
     const transcript = await transcribe(`${outputPath}/${id}-audio.mp3`);
-    console.log({ transcript });
+    console.log('Transcript Generated');
+    const vtt = generateVTT(transcript);
+    fs.writeFileSync(`${outputPath}/${id}.vtt`, vtt);
+    console.log('VTT Generated');
   } catch (processingError) {
     console.log({ processingError });
   } finally {
@@ -83,4 +109,4 @@ const generateVtt = async (mediaURL: string) => {
   }
 };
 
-generateVtt('https://static.scencdn.com/test_video.mp4');
+transcribeVideo('https://static.scencdn.com/test_video.mp4');
