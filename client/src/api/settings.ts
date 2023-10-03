1733bae180d19156798b6458ee538aa13071759a
taylorjackson <taylor.jackson@me.com>
2023-10-02 17:18:58 -0700

    test

--- client/src/api/settings.ts
+++ client/src/api/settings.ts
@@ -1,12 +1,10 @@
 export const CLOUDFLARE_SERVICE_URL: string =
-  window.__RUNTIME_CONFIG__.REACT_APP_CLOUDFLARE_SERVICE_URL ||
-  process.env.REACT_APP_CLOUDFLARE_SERVICE_URL ||
-  '';
+  // window.__RUNTIME_CONFIG__.REACT_APP_CLOUDFLARE_SERVICE_URL ||
+  process.env.REACT_APP_CLOUDFLARE_SERVICE_URL || '';
 
 export const BFF_URL: string =
-  window.__RUNTIME_CONFIG__.REACT_APP_BFF_URL ||
-  process.env.REACT_APP_BFF_URL ||
-  '';
+  // window.__RUNTIME_CONFIG__.REACT_APP_BFF_URL ||
+  process.env.REACT_APP_BFF_URL || '';
 
 export const DEFAULT_API_VERSION = 'v1';
 // export const API_BASE_PATH = `/api/${DEFAULT_API_VERSION}`;
