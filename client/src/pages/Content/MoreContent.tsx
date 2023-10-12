137673b5772ebeef8e37371758b5a66717d68c83
Ian Johnston <ianallanjohnston@gmail.com>
2023-10-12 01:36:40 -0700

    using new hook to get more content from current video's creator

--- client/src/pages/Content/MoreContent.tsx
+++ client/src/pages/Content/MoreContent.tsx
@@ -1,64 +1,33 @@
 import { Stack } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import ContentList from 'components/ContentList';
-import useContent from 'hooks/useContent';
 import { ContentLoader } from 'components/Loaders';
 import { PropsWithChildren, useEffect, useState } from 'react';
-import { getProfileId } from 'utils/auth';
-import { contentApi } from 'api';
-// import { getContentByProfileId } from 'api/content';
-// import { Content } from 'types/content';
+import useProfileContent from 'hooks/useProfileContent';
+
 interface MoreContentProps {
   profileId: string;
+  excludeId: string;
 }
 
-const MoreContent = ({ profileId }: PropsWithChildren<MoreContentProps>) => {
+const MoreContent = ({
+  profileId,
+  excludeId
+}: PropsWithChildren<MoreContentProps>) => {
   const [moreContent, setMoreContent] = useState<any>([]);
   const { t } = useTranslation();
-  const { data, isLoading } = useContent();
+  const { data: content, isLoading } = useProfileContent(profileId);
 
   useEffect(() => {
-    contentApi.getContentByProfileid(0, 10, profileId).then((res) => {
-      setMoreContent(res.data);
-      console.log(res.data);
-    });
-  }, []);
+    if (content) {
+      setMoreContent(content);
+      const filteredContent = excludeId
+        ? moreContent.filter((item: any) => item.id !== excludeId)
+        : content;
+      setMoreContent(filteredContent);
+    }
+  }, [content, excludeId]);
 
-  // const dummyContent: Content[] = [
-  //   {
-  //     id: 1,
-  //     thumbnailUrl:
-  //       'https://images.unsplash.com/photo-1515405295579-ba7b45403062?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=760&q=80',
-  //     title: 'Dummy Content 1',
-  //     creator: 'Creator 1',
-  //     url: 'https://example.com/content/1',
-  //     icon: 'https://dummyimage.com/32x32/000/fff',
-  //     hasSignLanguage: true
-  //   },
-  //   {
-  //     id: 2,
-  //     thumbnailUrl:
-  //       'https://images.unsplash.com/photo-1536924940846-227afb31e2a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=766&q=80',
-  //     title: 'Dummy Content 2',
-  //     creator: 'Creator 2',
-  //     url: 'https://example.com/content/2',
-  //     icon: 'https://dummyimage.com/32x32/000/fff',
-  //     hasSignLanguage: false
-  //   },
-  //   {
-  //     id: 3,
-  //     thumbnailUrl:
-  //       'https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=724&q=80',
-  //     title: 'Dummy Content 3',
-  //     creator: 'Creator 3',
-  //     url: 'https://example.com/content/3',
-  //     icon: 'https://dummyimage.com/32x32/000/fff',
-  //     hasSignLanguage: true
-  //   }
-  // ];
-
-  // const content = data ?? [];
-  // console.log(content);
   return (
     <Stack>
       {!isLoading && moreContent ? (
