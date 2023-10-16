1e3d50f2b4212bb82509e1291623748ae2f960e5
Ian Johnston <ianallanjohnston@gmail.com>
2023-10-15 22:49:06 -0700

    One requested change, removing FC old way and running prettier

--- client/src/components/PDFReader/PDFReader.tsx
+++ client/src/components/PDFReader/PDFReader.tsx
@@ -1,4 +1,3 @@
-import { FC } from 'react';
 import { useState } from 'react';
 import { pdfjs, Document, Page } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
@@ -8,7 +7,7 @@ import type { PDFDocumentProxy } from 'pdfjs-dist';
 
 const options = {
   cMapUrl: '/cmaps/',
-  standardFontDataUrl: '/standard_fonts/',
+  standardFontDataUrl: '/standard_fonts/'
 };
 interface PDFReaderProps {
   url: string;
@@ -16,20 +15,31 @@ interface PDFReaderProps {
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
+  import.meta.url
 ).toString();
 
-const PDFReader: FC<PDFReaderProps> = ({ url }) => {
+const PDFReader = ({ url }: PDFReaderProps) => {
   const [numPages, setNumPages] = useState<number>();
-  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
+  function onDocumentLoadSuccess({
+    numPages: nextNumPages
+  }: PDFDocumentProxy): void {
     setNumPages(nextNumPages);
   }
   return (
-    <Document className="pdfDocument" file={url} onLoadSuccess={onDocumentLoadSuccess} options={options}>
-    {Array.from(new Array(numPages), (el, index) => (
-      <Page className="pdfPage" key={`page_${index + 1}`} pageNumber={index + 1} />
-    ))}
-  </Document>
+    <Document
+      className="pdfDocument"
+      file={url}
+      onLoadSuccess={onDocumentLoadSuccess}
+      options={options}
+    >
+      {Array.from(new Array(numPages), (el, index) => (
+        <Page
+          className="pdfPage"
+          key={`page_${index + 1}`}
+          pageNumber={index + 1}
+        />
+      ))}
+    </Document>
   );
 };
 
