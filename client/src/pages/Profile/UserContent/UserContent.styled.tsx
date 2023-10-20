610fa8fc2d41d703afee260c32e4d6d15a7c8377
Lance Cummings <lance@glance.ca>
2023-10-20 08:28:19 -0700

    min height on user profile content area

--- client/src/pages/Profile/UserContent/UserContent.styled.tsx
+++ client/src/pages/Profile/UserContent/UserContent.styled.tsx
@@ -11,6 +11,7 @@ export const UserContent = styled(Box)`
     display: flex;
     flex-flow: row wrap;
     margin: -15px;
+    min-height: 600px;
 
     .content-card {
       flex: 0 0 calc(33.3333% - 30px);
