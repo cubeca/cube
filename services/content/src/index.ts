163b151fc66c9d7d3457951490ae268ba8559cfc
Adam Skalicky <adam.skalicky@scencdn.com>
2023-11-09 23:04:53 -0800

    Added message queuing for VTT generation

--- services/content/src/index.ts
+++ services/content/src/index.ts
@@ -1,4 +1,7 @@
 import express, { Express, Request, Response } from 'express';
+const { PubSub } = require('@google-cloud/pubsub');
+const pubsub = new PubSub();
+
 import cors from 'cors';
 import * as db from './db/queries';
 import * as settings from './settings';
@@ -23,7 +26,7 @@ const getApiResultFromDbRow = (r: any) => ({
 app.post('/content', allowIfAnyOf('contentEditor'), async (req: Request, res: Response) => {
   try {
     const user = extractUser(req);
-    const { profileId, ...contentData } = req.body;
+    const { profileId, vttFileId, type, ...contentData } = req.body;
 
     // Validate request body
     if (!profileId || Object.keys(contentData).length === 0) {
@@ -38,6 +41,16 @@ app.post('/content', allowIfAnyOf('contentEditor'), async (req: Request, res: Re
 
     // Insert content into database
     const dbResult = await db.insertContent({ profileId, ...contentData });
+
+    if (!vttFileId && (type === 'video' || type === 'audio')) {
+      // Publish a message to Google Pub/Sub
+      const topicName = 'vtt_transcribe'; // Replace with your actual topic name
+      const message = dbResult.id.toString(); // Assuming dbResult.id is the ID you want to publish
+
+      await pubsub.topic(topicName).publish(Buffer.from(message));
+
+      console.log('Queued VTT');
+    }
 
     return res.status(201).json(getApiResultFromDbRow(dbResult));
   } catch (error) {
