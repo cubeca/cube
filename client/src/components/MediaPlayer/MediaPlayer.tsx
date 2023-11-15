e57cb7e8ae0107a03c758341ee55ebb9d857c285
Ian Johnston <ianallanjohnston@gmail.com>
2023-11-14 21:57:23 -0800

    Changed syntax away from FC style.  Added subtitle track, removed check for isAudio because we want the video player to remain tall even when audio is playing to display cover art

--- client/src/components/MediaPlayer/MediaPlayer.tsx
+++ client/src/components/MediaPlayer/MediaPlayer.tsx
@@ -1,24 +1,54 @@
 import { FC } from 'react';
 import ReactPlayer from 'react-player';
+import './MediaPlayer.css';
 
 interface MediaPlayerProps {
   url: string;
   isAudio?: boolean;
+  coverArtUrl?: string;
+  subtitleUrl?: string;
+  isSafari?: boolean;
 }
 
-const MediaPlayer: FC<MediaPlayerProps> = ({ url, isAudio }) => {
+const MediaPlayer = ({
+  url,
+  isAudio,
+  coverArtUrl,
+  subtitleUrl,
+  isSafari
+}: MediaPlayerProps) => {
   return (
     <ReactPlayer
       url={url}
+      style={{
+        backgroundImage: isSafari ? `url('${coverArtUrl}')` : undefined,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        zIndex: 1,
+        height: '100%'
+      }}
       config={{
         file: {
-          forceVideo: !isAudio,
-          forceAudio: isAudio
+          attributes: {
+            poster: coverArtUrl,
+            controls: true
+          },
+          forceVideo: true,
+          tracks: [
+            {
+              src: subtitleUrl || '',
+              kind: 'subtitles',
+              srcLang: 'en',
+              default: true,
+              label: 'English'
+            }
+          ]
         }
       }}
       controls
       width={'100%'}
-      height={isAudio ? '40px' : 'auto'}
+      height={'auto'}
     />
   );
 };
