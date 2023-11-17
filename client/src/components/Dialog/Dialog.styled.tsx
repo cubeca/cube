cfbd198b65784c4e5a3f9b249f6a72cdeb9e1e84
Lance Cummings <lance@glance.ca>
2023-11-17 13:07:46 -0800

    styled embed dialogue

--- client/src/components/Dialog/Dialog.styled.tsx
+++ client/src/components/Dialog/Dialog.styled.tsx
@@ -13,6 +13,7 @@ export const Title = styled(DialogTitle)`
   width: 600px;
   max-width: 100%;
   background-color: ${(props) => props.theme.palette.primary.main};
+  color: ${(props) => props.theme.palette.background.default};
   
   ${(props) => props.theme.breakpoints.up('sm')} {
   }
--- client/src/components/EmbedModal/EmbedModal.styled.tsx
+++ client/src/components/EmbedModal/EmbedModal.styled.tsx
@@ -21,12 +21,37 @@ export const Close = styled(IconButton)`
 `;
 
 export const EmbedTextField = styled(TextField)`
+
+  .MuiInputBase-root {
+    margin-bottom: 10px;
+  }
+
   & label {
-    color: ${(props) => props.theme.palette.background.default};
     display: block;
     font-size: 1rem;
     font-weight: 500;
     margin-bottom: 0;
     font-weight: 600;
+  }
+
+  &.dark {
+    & input,
+    & fieldset,
+    & input:focus + fieldset,
+    & input:active + fieldset,
+    & input:hover + fieldset {
+      border-color: ${(props) =>
+        props.theme.palette.background.default} !important;
+    }
+
+    & input,
+    & textarea {
+      color: ${(props) => props.theme.palette.background.default};
+    }
+
+    & label {
+      color: ${(props) => props.theme.palette.background.default};
+    }
+
   }
 `;
