966fb87bcdabbab8f2d7fbd3c04958da0a154406
taylorjackson <taylor.jackson@me.com>
2023-11-28 11:18:38 -0800

    modal fix

--- client/src/components/EmbedModal/EmbedModal.tsx
+++ client/src/components/EmbedModal/EmbedModal.tsx
@@ -16,13 +16,13 @@ function generateEmbedCode(type: string, url: string) {
 
   switch (type) {
     case 'audio':
-      embedCode = `<iframe src="${url}" style="width:100%; height:auto;" frameborder="0"></iframe>`;
+      embedCode = `<iframe src="${url}" style="width:100%; height:100%;" frameborder="0"></iframe>`;
       break;
     case 'pdf':
-      embedCode = `<iframe src="${url}" style="width:100%; height:auto;" frameborder="0"></iframe>`;
+      embedCode = `<iframe src="${url}" style="width:100%; height:100%;" frameborder="0"></iframe>`;
       break;
     case 'video':
-      embedCode = `<iframe src="${url}" style="width:100%; height:auto;" frameborder="0"></iframe>`;
+      embedCode = `<iframe src="${url}" style="width:100%; height:100%;" frameborder="0"></iframe>`;
       break;
     case 'link':
       embedCode = `<a href="${url}" target="_blank"></a>`;
