8f43e951e755ee29dc03dc4eb240869d3656486a
Ian Johnston <ianallanjohnston@gmail.com>
2023-12-16 12:38:09 -0800

    created alternate RadioInput for long wrapping text
    It's annoying that there doesn't seem to be a simpler way to achieve this with CSS or a prop in the MUI RadioButton but this is at least a workaround for now, unless someone knows of a cleaner way

--- client/src/components/form/RadioInput/RadioInput.styled.tsx
+++ client/src/components/form/RadioInput/RadioInput.styled.tsx
@@ -14,3 +14,22 @@ export const DarkRadioInput = styled(RadioInput)`
     color: ${(props) => props.theme.palette.background.default};
   }
 `;
+
+export const WrappingDarkRadioInput = styled(RadioInput)`
+  & label,
+  .Mui-focused {
+    color: ${(props) => props.theme.palette.background.default};
+    display: flex;
+    font-size: 1rem;
+    font-weight: 400;
+    margin-bottom: 0;
+    position: relative;
+    padding-left: 50px;
+  }
+  .MuiRadio-root {
+    color: ${(props) => props.theme.palette.background.default};
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+`;
