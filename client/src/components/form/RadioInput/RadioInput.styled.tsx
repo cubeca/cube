701e357944a6d490a2379852f82fd26a7cd34d13
Ian Johnston <ianallanjohnston@gmail.com>
2023-12-16 12:09:56 -0800

    adjusted font weight for radio inputs

--- client/src/components/form/RadioInput/RadioInput.styled.tsx
+++ client/src/components/form/RadioInput/RadioInput.styled.tsx
@@ -2,13 +2,13 @@ import { styled } from 'theme/utils';
 import RadioInput from './RadioInput';
 
 export const DarkRadioInput = styled(RadioInput)`
-  & label, .Mui-focused {
+  & label,
+  .Mui-focused {
     color: ${(props) => props.theme.palette.background.default};
     display: block;
     font-size: 1rem;
-    font-weight: 500;
+    font-weight: 400;
     margin-bottom: 0;
-    font-weight: 600;
   }
   .MuiRadio-root {
     color: ${(props) => props.theme.palette.background.default};
