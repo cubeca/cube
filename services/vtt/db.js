22e7aa5fb935ae8ab24497726dc7985f3b9f93db
Adam Skalicky <adam.skalicky@scencdn.com>
2024-01-19 17:28:14 -0800

    corrected db

--- services/vtt/db.js
+++ services/vtt/db.js
@@ -1,102 +1,95 @@
 const { COCKROACH_DB_CONNECTION_STRING } = require("./settings");
-const { DataTypes, Model } = require("sequelize");
+const { DataTypes } = require("sequelize");
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(COCKROACH_DB_CONNECTION_STRING, {
   schema: "cube",
 });
 
-class vtt extends Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        id: {
-          type: DataTypes.UUID,
-          defaultValue: DataTypes.UUIDV4,
-          primaryKey: true,
-        },
-        transcript: {
-          type: DataTypes.JSONB,
-          allowNull: false,
-          defaultValue: {},
-        },
-      },
-      {
-        sequelize,
-        modelName: "vtt",
-        tableName: "vtt",
-      }
-    );
+const vtt = sequelize.define(
+  "vtt",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    transcript: {
+      type: DataTypes.JSONB,
+      defaultValue: {},
+    },
+  },
+  {
+    sequelize,
+    modelName: "vtt",
+    tableName: "vtt",
+    timestamps: false,
   }
-}
+);
 
-class content extends Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        id: {
-          type: DataTypes.UUID,
-          defaultValue: DataTypes.UUIDV4,
-          primaryKey: true,
-        },
-        created_at: {
-          type: DataTypes.DATE,
-          defaultValue: DataTypes.NOW,
-        },
-        updated_at: {
-          type: DataTypes.DATE,
-          defaultValue: DataTypes.NOW,
-        },
-        data: {
-          type: DataTypes.JSONB,
-          allowNull: false,
-          defaultValue: {},
-        },
-      },
-      {
-        sequelize,
-        modelName: "content",
-        tableName: "content",
-      }
-    );
+const content = sequelize.define(
+  "content",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    data: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      defaultValue: {},
+    },
+  },
+  {
+    sequelize,
+    modelName: "content",
+    tableName: "content",
+    timestamps: false,
   }
-}
+);
 
-class files extends Model {
-  static init(sequelize) {
-    return super.init(
-      {
-        id: {
-          type: DataTypes.UUID,
-          defaultValue: DataTypes.UUIDV4,
-          primaryKey: true,
-        },
-        storage_type: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        created_at: {
-          type: DataTypes.DATE,
-          defaultValue: DataTypes.NOW,
-        },
-        updated_at: {
-          type: DataTypes.DATE,
-          defaultValue: DataTypes.NOW,
-        },
-        data: {
-          type: DataTypes.JSONB,
-          allowNull: false,
-          defaultValue: {},
-        },
-      },
-      {
-        sequelize,
-        modelName: "files",
-        tableName: "files",
-      }
-    );
+const files = sequelize.define(
+  "files",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    storage_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    data: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      defaultValue: {},
+    },
+  },
+  {
+    sequelize,
+    modelName: "files",
+    tableName: "files",
+    timestamps: false,
   }
-}
+);
 
 module.exports = {
   sequelize,
