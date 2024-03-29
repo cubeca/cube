b7adabec8bafb6c6927b459b80635a6c3237c08a
Ian Johnston <ianallanjohnston@gmail.com>
2024-03-29 02:43:44 -0700

    reverting change to check formstate in editmode for now :) it's late, i will sleep

--- client/src/pages/Upload/Upload.tsx
+++ client/src/pages/Upload/Upload.tsx
@@ -466,9 +466,9 @@ const Upload = () => {
           handleSubmit={handleSubmit(onSubmit)}
           editMode={editMode}
           isNextDisabled={
-            (editMode && !formState.isValid) ||
-            (!editMode &&
-              (!formState.isValid ||
+            editMode
+              ? false
+              : !formState.isValid ||
                 (screenIndex === 0 &&
                   mediaType === ('video' || 'audio' || 'pdf' || 'document') &&
                   !isMediaProperFileType) ||
@@ -481,7 +481,7 @@ const Upload = () => {
                 (!mediaLink && !isMediaSelected) ||
                 (screenIndex === 1 &&
                   !isVTTSelected &&
-                  ['video', 'audio'].includes(mediaType))))
+                  mediaType in ['video', 'audio'])
           }
         />
       )}
