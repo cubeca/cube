8dd2694d644fcf733b16e0e25a7d9753365f88eb
Ian Johnston <ianallanjohnston@gmail.com>
2024-08-12 22:06:50 +0300

    additional fix for pdfjs import

--- client/src/components/PDFReader/FullscreenModal.tsx
+++ client/src/components/PDFReader/FullscreenModal.tsx
@@ -11,10 +11,7 @@ import {
 import { Box } from '@mui/material';
 import * as s from './PDFReader.styled';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 interface PDFModalProps {
   url: string;
